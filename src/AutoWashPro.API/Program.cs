3a7432437a437843dc197e20db9aaba40076d662
Noted <ltanloc212@gmail.com>
2026-06-10 23:37:45 +0700

    Commit #6: Update Program.cs for issue 08 task

--- src/AutoWashPro.API/Program.cs
+++ src/AutoWashPro.API/Program.cs
@@ -68,6 +68,11 @@ builder.Services.AddScoped<ITierService, TierService>();
 builder.Services.AddScoped<TierRepository>();
 builder.Services.AddHostedService<TierDowngradeJob>();
 
+// ISSUE-08: Admin Booking Workflow
+builder.Services.AddScoped<IAdminBookingService, AdminBookingService>();
+builder.Services.AddHostedService<AutoNoShowJob>();
+builder.Services.AddHostedService<SuspensionCheckJob>();
+
 builder.Services.AddAuthorization();
 var app = builder.Build();
 
