209cb8dfbc3606c610ded4f100f4ecce3331e54c
Noted <ltanloc212@gmail.com>
2026-06-11 00:37:51 +0700

    Commit #10: Update IPointService.cs for issue 09 task

---
+++ src/AutoWash.Application/Interfaces/IPointService.cs
@@ -0,0 +1,10 @@
+using System.Threading.Tasks;
+
+namespace AutoWash.Application.Interfaces
+{
+    public interface IPointService
+    {
+        Task<int> EarnPointsAsync(int bookingId);
+        Task RedeemPointsAsync(int bookingId);
+    }
+}
