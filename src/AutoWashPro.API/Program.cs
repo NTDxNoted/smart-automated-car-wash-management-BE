0ee6412f554629437c81189d545a6bb6ae01ffa0
Noted <ltanloc212@gmail.com>
2026-06-12 09:45:51 +0700

    [HOTFIX] Allow all origins in CORS policy

--- src/AutoWashPro.API/Program.cs
+++ src/AutoWashPro.API/Program.cs
@@ -21,7 +21,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("FrontendDev", policy =>
     {
-        policy.WithOrigins("http://localhost:5173", "http://localhost:3000")
+        policy.SetIsOriginAllowed(origin => true)
               .AllowAnyHeader()
               .AllowAnyMethod()
               .AllowCredentials();
